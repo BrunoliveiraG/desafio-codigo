797318a958be5f12ebd5a53395589d82db5ee3af
BrunoliveiraG <brunoliveiragomesz@gmail.com>
2021-06-28 20:59:31 -0300

    Adicionando biblioteca de tradução e modularizando

--- kudos.js
+++ kudos.js
@@ -1,4 +1,4 @@
-// Conversão de kudos para pontos
+// Conversion from kudos to points
 const KUDOS_TO_POINTS = [
   { name: 'OK', value: 5 },
   { name: 'NICE', value: 10 },
@@ -7,7 +7,7 @@ const KUDOS_TO_POINTS = [
   { name: 'SUPER', value: 100 },
 ];
 
-// Conversão de kudos para reais
+// Conversion from kudos to points
 const KUDOS_TO_REAL = [
   { name: 'OK', value: 2 },
   { name: 'NICE', value: 5 },
@@ -16,65 +16,85 @@ const KUDOS_TO_REAL = [
   { name: 'SUPER', value: 25 },
 ];
 
-//A nossa função de conversão para extenso
-const extenso = require('./extenso');
+// Conversion to written number
+var writtenNumber = require('written-number');
 
 /* 
-  Recebe: um int representando o número de pontos do usuário
-  Retorna: um array contendo os kudos. Ex.: ['OK', 'GOOD'] 
+  Receives: integer representing user points
+  Returns: String with value and kudos
 */
 function getKudosForUser(points) {
-  //Valor total recebido
-  let valorTotal = 0;
 
-  //Lista para guardar os kudos recebidos
-  let kudos = [];
+  let kudos = getKudosList(points);
+  let totalValue = getKudosValue(points);
 
-  let listaToPoints = (Object.values(KUDOS_TO_POINTS));
-  let listaToReal = (Object.values(KUDOS_TO_REAL));
-  /*
-  Aqui percorremos as duas listas ao mesmo tempo, fazendo os calculos
-  Isso permite que no futuro possamos adicionar novos tipos de Kudos alterando somente o Objeto
-   */
-  for(let i=listaToPoints.length-1; i >= 0 ; i--){
-
-    let count = 0;
-
-    //verificamos se tem pontos suficientes pro kudos atual
-    //atualizamos os pontos para o %(resto) da divisão
-    count = Math.floor(points / listaToPoints[i].value);
-    points %= listaToPoints[i].value;
-
-    //adicionamos o kudos *(vezes) quantas vezes ele aparece ao total
-    valorTotal += count * listaToReal[i].value;
-
-    //usamos a mesma count para adicionar o Kudos (N) vezes à lista
-    for(let j = 0; j<count; j++){
-      kudos.push(listaToPoints[i].name);
-    }
-
-  }
-
-  let resposta;
-  //Montamos a resposta com a nossa funcão de transformar o valor em extenso
-  //E damos join com os kudos da lista de kudos que montamos
-  if(valorTotal > 0 && valorTotal < 1000000){
-    let valorExtenso = extenso.getNumeroPorExtenso(valorTotal);
-    resposta = "Você recebeu " + valorExtenso + " reais em retorno aos kudos " + kudos.join(', ') + "!";
-  }//Tratamos se o valor é zero ou maior que 999999
-  else if(valorTotal === 0){
-    resposta = "Você recebeu " + "zero" + " reais em retorno aos kudos" +  "!";
-  }
-  else{
-    resposta = "Você recebeu " + "um milhão de" + " reais em retorno aos kudos " + kudos.join(', ') + "!";
-  }
-
-  return resposta;
-
+  return createResponse(kudos, totalValue);
 }
 
-// Usamos uma função única com 1 loop for para otimizar o código
-// function getKudosValueMessageForUser(kudos) {}
+/*
+Receives: integer representing user points
+Returns: total money earned from kudos
+ */
+function getKudosValue(points){
+  let totalValue = 0;
+  let listToReal = (Object.values(KUDOS_TO_REAL));
+  let listToPoints = (Object.values(KUDOS_TO_POINTS));
+
+  for(let i=listToReal.length-1; i >= 0 ; i--) {
+    
+    totalValue += getKudoAmmount(points, i) * listToReal[i].value;
+    points %= listToPoints[i].value;
+  }
+  return totalValue;
+}
+
+/*
+Receives: integer representing user points
+Returns: array with all the kudos received
+ */
+function getKudosList(points){
+  let kudos = [];
+  let listToPoints = (Object.values(KUDOS_TO_POINTS));
+
+  for(let i=listToPoints.length-1; i >= 0 ; i--){
+
+    for(let j = 0; j<getKudoAmmount(points, i); j++){
+      kudos.push(listToPoints[i].name);
+    }
+    points %= listToPoints[i].value;
+  }
+  return kudos;
+}
+
+/*
+Receives: integer representing remainder of user points
+Returns: Ammount of times a kudo enters the list
+ */
+function getKudoAmmount(points, i){
+  let listToPoints = (Object.values(KUDOS_TO_POINTS));
+  return Math.floor(points / listToPoints[i].value);
+}
+
+/*
+Receives: list of kudos and total value earned
+Returns: Call to format the String depending on the value (Max: 1mil)
+ */
+function createResponse(kudos, totalValue) {
+  if(totalValue >= 0 && totalValue <= 1000000){
+    let writtenValue = writtenNumber(totalValue, {lang: 'pt'});
+    return createKudosMessage(writtenValue, kudos)
+  }
+  return createKudosMessage("um milhão de", kudos);
+}
+
+/*
+Receives: list of kudos and total value earned treated
+Returns: Formated string with answer
+ */
+function createKudosMessage(value, kudos) {
+  kudosFormatted =  kudos.join(', ');
+  return `Você recebeu ${value} reais em retorno aos kudos ${kudosFormatted}!`;
+}
 
 module.exports = {
   getKudosForUser,
--- kudos.test.js
+++ kudos.test.js
@@ -1,7 +1,7 @@
 const kudos = require('./kudos');
 
 test('test getKudosForUser', () => {
-  expect(kudos.getKudosForUser(0)).toEqual("Você recebeu zero reais em retorno aos kudos!");
+  expect(kudos.getKudosForUser(0)).toEqual("Você recebeu zero reais em retorno aos kudos !");
   expect(kudos.getKudosForUser(30)).toEqual("Você recebeu treze reais em retorno aos kudos GOOD, NICE!");
   expect(kudos.getKudosForUser(135)).toEqual("Você recebeu quarenta reais em retorno aos kudos SUPER, GOOD, NICE, OK!");
   expect(kudos.getKudosForUser(40)).toEqual("Você recebeu dezesseis reais em retorno aos kudos GOOD, GOOD!");
